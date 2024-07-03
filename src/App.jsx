0cce5a50ba500527ae2c0548c97168db0eedefa3
faruh121 <yussupov.fara@gmail.com>
2024-07-03 13:20:30 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+function Header(){
+  return ( 
+    <header>
+      <h3>ТУТ БУдет хедер</h3>
+      
+      </header>
+   )
+}
+
+
+export default function app(){
+  return(
+    <div>
+      <Header />
+      <main>
+      <h3>FARA LEARN REACT</h3>
+      <marquee>....................................</marquee>
+      </main>
+    </div>
+    
+  )
+}
+
+
